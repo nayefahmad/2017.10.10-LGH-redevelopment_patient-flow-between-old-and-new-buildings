8e1bf47a516e3474440553889db12ae90a5ea219
nayefahmad <nayef.ahmad.11@gmail.com>
2017-10-11 10:25:56 -0700

    fix histogram - xlim to -1, colour, print location

--- src/2010-10-06_LGH_analysis-of-LOS.R
+++ src/2010-10-06_LGH_analysis-of-LOS.R
@@ -58,9 +58,10 @@ p1_hist <-
              aes(x=los.4e)) + 
       geom_histogram(stat="bin", 
                      binwidth = 1, 
-                     col="black") + 
+                     col="black", 
+                     fill="deepskyblue") + 
       
-      scale_x_continuous(limits=c(0,85), 
+      scale_x_continuous(limits=c(-1,85), 
                          breaks=seq(0,85,5), 
                          expand=c(0,0)) + 
       scale_y_continuous(expand=c(0,0)) + 
@@ -70,13 +71,13 @@ p1_hist <-
            title="Distribution of LOS in LGH 4E", 
            subtitle="From 2014-04-01 onwards \nMedian = 5 days; Mean = 11.1 days; ", 
            caption= "\nData source: DSDW ADTCMart; extraction date: 2017-10-10 ") + 
+      
       geom_vline(xintercept = avg.los, 
                  col="red") + 
       
       geom_vline(xintercept = median.los, 
                  col="red", 
                  linetype=2) + 
-      
       
       theme_classic(base_size = 16); p1_hist
 
@@ -85,7 +86,7 @@ p1_hist <-
 # *******************************************
 # Save outputs: ---------------
 
-pdf(file="\\\\vch.ca/departments/Projects (Dept VC)/Patient Flow Project/Coastal HSDA/2017 Requests/2017.10.04 LGH redevelopment - patient flow between old and new buildings/results/output from src/los-histogram-2015-2017.pdf", 
+pdf(file="\\\\vch.ca/departments/Projects (Dept VC)/Patient Flow Project/Coastal HSDA/2017 Requests/2017.10.10 LGH redevelopment - patient flow between old and new buildings/results/output from src/los-histogram-2015-2017.pdf", 
       height= 8.5, width = 14) 
 p1_hist
 dev.off()
